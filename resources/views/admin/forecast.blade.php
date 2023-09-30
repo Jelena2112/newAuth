de0f95a172399a690033d8398f5877fc49ff43a7
ITMentorstva <117232761+ITMentorstva@users.noreply.github.com>
2023-09-30 22:02:45 +0200

    Added Layout for admin & bootstrap 5 , Fontawesome

--- resources/views/admin/forecast.blade.php
+++ resources/views/admin/forecast.blade.php
@@ -1,34 +1,42 @@
-<form method="POST" action="{{ route('admin.add.forecast') }}">
-    {{ csrf_field() }}
+@extends("admin.layoutAdmin")
 
-    <input name="temperature" type="number" placeholder="Unesite temp.">
-    <input name="probability" type="number" placeholder="Verovatnoca padavina">
-    <input name="forecast_date" type="date">
+@section("content")
 
-    <select name="weather_type">
-        @foreach(\App\Models\ForecastModel::WEATHER as $weather)
-            <option value="{{ $weather }}">{{ $weather }}</option>
-        @endforeach
-    </select>
+    <form method="POST" action="{{ route('admin.add.forecast') }}">
+        {{ csrf_field() }}
 
-    <select name="city_id">
-        @foreach(\App\Models\CitiesModel::all() as $city)
-            <option value="{{ $city->id }}">{{ $city->name }}</option>
-        @endforeach
-    </select>
+        <input name="temperature" type="number" placeholder="Unesite temp.">
+        <input name="probability" type="number" placeholder="Verovatnoca padavina">
+        <input name="forecast_date" type="date">
 
-    <button>Snimi</button>
-</form>
+        <select name="weather_type">
+            @foreach(\App\Models\ForecastModel::WEATHER as $weather)
+                <option value="{{ $weather }}">{{ $weather }}</option>
+            @endforeach
+        </select>
 
-@foreach(\App\Models\CitiesModel::all() as $city)
-    <p>{{ $city->name }}</p>
-    <ul>
-        @foreach($city->forecast as $forecast)
-            @php
-                $color = \App\Http\ForecastHelper::getColorByTemp($forecast->temperature);
-            @endphp
-            <li>Date: {{ $forecast->forecast_date }} - <span style="color : {{ $color }}"> Temperature: {{ $forecast->temperature }}</span> </li>
-        @endforeach
-    </ul>
+        <select name="city_id">
+            @foreach(\App\Models\CitiesModel::all() as $city)
+                <option value="{{ $city->id }}">{{ $city->name }}</option>
+            @endforeach
+        </select>
 
-@endforeach
+        <button>Snimi</button>
+    </form>
+
+    @foreach(\App\Models\CitiesModel::all() as $city)
+        <p>{{ $city->name }}</p>
+        <ul>
+            @foreach($city->forecast as $forecast)
+                @php
+                    $color = \App\Http\ForecastHelper::getColorByTemp($forecast->temperature);
+                @endphp
+                <li>Date: {{ $forecast->forecast_date }} - <span style="color : {{ $color }}"> Temperature: {{ $forecast->temperature }}</span> </li>
+            @endforeach
+        </ul>
+
+    @endforeach
+
+@endsection
+
+
