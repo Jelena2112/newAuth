733fbbbfb9b0bf5713788bc8693f06955bab4e6d
ITMentorstva <117232761+ITMentorstva@users.noreply.github.com>
2023-09-30 22:26:24 +0200

    Frontend for admin forecast view

--- resources/views/admin/forecast.blade.php
+++ resources/views/admin/forecast.blade.php
@@ -2,40 +2,56 @@
 
 @section("content")
 
-    <form method="POST" action="{{ route('admin.add.forecast') }}">
+    <form class="d-flex col-8 p-3 m-2 flex-column" method="POST" action="{{ route('admin.add.forecast') }}">
+
+        <h4 class="m-3">Kreirenje nove prognoze</h4>
+
         {{ csrf_field() }}
+        <div class="mb-3 col-4">
+            <input class="form-control" name="temperature" type="number" placeholder="Unesite temp.">
+        </div>
+        <div class="mb-3 col-4">
+            <input class="form-control" name="probability" type="number" placeholder="Verovatnoca padavina">
+        </div>
+        <div class="mb-3 col-4">
+            <input class="form-control" name="forecast_date" type="date">
+        </div>
+        <div class="mb-3 col-4">
+            <select name="weather_type" class="form-select">
+                @foreach(\App\Models\ForecastModel::WEATHER as $weather)
+                    <option value="{{ $weather }}">{{ $weather }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-3 col-4">
+            <select name="city_id" class="form-select">
+                @foreach(\App\Models\CitiesModel::all() as $city)
+                    <option value="{{ $city->id }}">{{ $city->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-3">
+            <button class="btn btn-primary">Snimi</button>
+        </div>
 
-        <input name="temperature" type="number" placeholder="Unesite temp.">
-        <input name="probability" type="number" placeholder="Verovatnoca padavina">
-        <input name="forecast_date" type="date">
-
-        <select name="weather_type">
-            @foreach(\App\Models\ForecastModel::WEATHER as $weather)
-                <option value="{{ $weather }}">{{ $weather }}</option>
-            @endforeach
-        </select>
-
-        <select name="city_id">
-            @foreach(\App\Models\CitiesModel::all() as $city)
-                <option value="{{ $city->id }}">{{ $city->name }}</option>
-            @endforeach
-        </select>
-
-        <button>Snimi</button>
     </form>
 
-    @foreach(\App\Models\CitiesModel::all() as $city)
-        <p>{{ $city->name }}</p>
-        <ul>
-            @foreach($city->forecast as $forecast)
-                @php
-                    $color = \App\Http\ForecastHelper::getColorByTemp($forecast->temperature);
-                @endphp
-                <li>Date: {{ $forecast->forecast_date }} - <span style="color : {{ $color }}"> Temperature: {{ $forecast->temperature }}</span> </li>
-            @endforeach
-        </ul>
+    <div class="d-flex flex-wrap">
+        @foreach(\App\Models\CitiesModel::all() as $city)
+            <div class="m-3">
+                <p class="mb-1 fw-bold">{{ $city->name }}</p>
+                <ul class="list-group mb-4 ">
+                    @foreach($city->forecast as $forecast)
+                        @php
+                            $color = \App\Http\ForecastHelper::getColorByTemp($forecast->temperature);
+                        @endphp
+                        <li class="list-group-item">Date: {{ $forecast->forecast_date }} - <span style="color : {{ $color }}"> Temperature: {{ $forecast->temperature }}</span> </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
+    </div>
 
-    @endforeach
 
 @endsection
 
