995af9e8d51fae9e12e28f5c40786d2d0318abb7
ITMentorstva <117232761+ITMentorstva@users.noreply.github.com>
2023-10-02 22:28:10 +0200

    Added like btn for searchResults

--- resources/views/searchResults.blade.php
+++ resources/views/searchResults.blade.php
@@ -2,13 +2,20 @@
 
 @section('content')
 
-    @foreach($search as $city)
+    <div class="mt-5 d-flex flex-wrap p-4">
+        @foreach($search as $city)
 
-        @php $icon = \App\Http\ForecastHelper::getIconByWeather($city->todaysForecast->weather_type) @endphp
+            @php $icon = \App\Http\ForecastHelper::getIconByWeather($city->todaysForecast->weather_type) @endphp
 
-        <p><a href="{{ route('forecast.any', ['city' => $city->name]) }}"> <i class="fas {{ $icon }}"></i> {{ $city->name }}</a></p>
+            <p class="col-2 m-2"> <i class="fas fa-thumbs-up btn btn-primary"></i>
+                <a class="btn-primary btn" href="{{ route('forecast.any', ['city' => $city->name]) }}">
+                    <i class="fas {{ $icon }}"></i> {{ $city->name }}
+                </a>
+            </p>
 
-    @endforeach
+        @endforeach
+    </div>
+
 
 @endsection
 
